85a4850a33ee134572d14642ac6215304df48362
Thomaz T. O. Toyama <thomazot@gmail.com>
2020-05-25 10:43:36 -0300

    Finalizado

---
+++ data/brands.js
@@ -0,0 +1,102 @@
+module.exports = [
+    {
+        name: "Verginia",
+        image: "./assets/brands/brand-01.jpg",
+        link: "#",
+    },
+    {
+        name: "Suvinil",
+        image: "./assets/brands/brand-02.jpg",
+        link: "#",
+    },
+    {
+        name: "Dacar",
+        image: "./assets/brands/brand-03.jpg",
+        link: "#",
+    },
+    {
+        name: "Sherwin Williams",
+        image: "./assets/brands/brand-04.jpg",
+        link: "#",
+    },
+    {
+        name: "",
+        image: "./assets/brands/brand-05.jpg",
+        link: "#",
+    },
+    {
+        name: "Lazzuril",
+        image: "./assets/brands/brand-06.jpg",
+        link: "#",
+    },
+    {
+        name: "Atlas",
+        image: "./assets/brands/brand-07.jpg",
+        link: "#",
+    },
+    {
+        name: "3M",
+        image: "./assets/brands/brand-08.jpg",
+        link: "#",
+    },
+    {
+        name: "Montana",
+        image: "./assets/brands/brand-09.jpg",
+        link: "#",
+    },
+    {
+        name: "Farben",
+        image: "./assets/brands/brand-10.jpg",
+        link: "#",
+    },
+    {
+        name: "Anjo",
+        image: "./assets/brands/brand-11.jpg",
+        link: "#",
+    },
+    {
+        name: "Saint Gobain",
+        image: "./assets/brands/brand-12.jpg",
+        link: "#",
+    },
+    {
+        name: "Vonder",
+        image: "./assets/brands/brand-13.jpg",
+        link: "#",
+    },
+    {
+        name: "Color Gin",
+        image: "./assets/brands/brand-14.jpg",
+        link: "#",
+    },
+    {
+        name: "Tigre",
+        image: "./assets/brands/brand-15.jpg",
+        link: "#",
+    },
+    {
+        name: "Vendacit",
+        image: "./assets/brands/brand-16.jpg",
+        link: "#",
+    },
+    {
+        name: "2001",
+        image: "./assets/brands/brand-17.jpg",
+        link: "#",
+    },
+    {
+        name: "Renner",
+        image: "./assets/brands/brand-18.jpg",
+        link: "#",
+    },
+    {
+        name: "Viapol",
+        image: "./assets/brands/brand-19.jpg",
+        link: "#",
+    },
+    {
+        name: "Hidronorth",
+        image: "./assets/brands/brand-20.jpg",
+        link: "#",
+    },
+]
---
+++ data/customers.js
@@ -0,0 +1,107 @@
+module.exports = [
+    {
+        name: "Universidade Positivo",
+        image: "./assets/customers/customer-01.jpg",
+        link: "#",
+    },
+    {
+        name: "PUCPR",
+        image: "./assets/customers/customer-02.jpg",
+        link: "#",
+    },
+    {
+        name: "Bom Jesus",
+        image: "./assets/customers/customer-03.jpg",
+        link: "#",
+    },
+    {
+        name: "Marista",
+        image: "./assets/customers/customer-04.jpg",
+        link: "#",
+    },
+    {
+        name: "Colégio Madianeira",
+        image: "./assets/customers/customer-05.jpg",
+        link: "#",
+    },
+    {
+        name: "Festval",
+        image: "./assets/customers/customer-06.jpg",
+        link: "#",
+    },
+    {
+        name: "Apolar Imóveis",
+        image: "./assets/customers/customer-07.jpg",
+        link: "#",
+    },
+    {
+        name: "PetrOfisa",
+        image: "./assets/customers/customer-08.jpg",
+        link: "#",
+    },
+    {
+        name: "Sanepar",
+        image: "./assets/customers/customer-09.jpg",
+        link: "#",
+    },
+    {
+        name: "Furukawa",
+        image: "./assets/customers/customer-10.jpg",
+        link: "#",
+    },
+    {
+        name: "Plaenge",
+        image: "./assets/customers/customer-11.jpg",
+        link: "#",
+    },
+    {
+        name: "Manhattan",
+        image: "./assets/customers/customer-12.jpg",
+        link: "#",
+    },
+    {
+        name: "Rottas",
+        image: "./assets/customers/customer-13.jpg",
+        link: "#",
+    },
+    {
+        name: "Sumitomo",
+        image: "./assets/customers/customer-14.jpg",
+        link: "#",
+    },
+    {
+        name: "Gonzaga Imóveis",
+        image: "./assets/customers/customer-15.jpg",
+        link: "#",
+    },
+    {
+        name: "Monreal",
+        image: "./assets/customers/customer-16.jpg",
+        link: "#",
+    },
+    {
+        name: "Speck",
+        image: "./assets/customers/customer-17.jpg",
+        link: "#",
+    },
+    {
+        name: "Êxito",
+        image: "./assets/customers/customer-18.jpg",
+        link: "#",
+    },
+    {
+        name: "Propex",
+        image: "./assets/customers/customer-19.jpg",
+        link: "#",
+    },
+    {
+        name: "Pelanda",
+        image: "./assets/customers/customer-20.jpg",
+        link: "#",
+    },
+    {
+        name: "Sial",
+        image: "./assets/customers/customer-21.jpg",
+        link: "#",
+    },
+]
